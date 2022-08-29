70e2708f820d3b067f71da56c54d06f17cd688d4
thordurf <thordur921@gmail.com>
2022-08-29 16:12:57 +0200

    solution

---
+++ capitalization3.js
@@ -0,0 +1,8 @@
+const firstName = "rasmus";
+
+const third = firstName.substring(2,3);
+
+const uppercase = firstName.substring(2,3).toUpperCase(); 
+
+console.log(firstName.replace(third, uppercase));
+
