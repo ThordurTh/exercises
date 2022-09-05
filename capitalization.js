28bb803ffeb9041e7cbc52fbd18f3324bbf28940
thordurf <thordur921@gmail.com>
2022-09-05 11:48:41 +0200

    string methods

---
+++ capitalization.js
@@ -0,0 +1,8 @@
+const firstName = "rasmus";
+
+const first = firstName.substring(0,1);
+
+const uppercase = firstName.substring(0,1).toUpperCase(); 
+
+console.log(firstName.replace(first, uppercase));
+
