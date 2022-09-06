8fdf38172223a46ba516803bcf69b7edb038d9bf
thordurf <thordur921@gmail.com>
2022-09-06 13:39:01 +0200

    Exercise 4 and 5

--- nameparts.js
+++ nameparts.js
@@ -30,18 +30,55 @@
 
 ///////// Third exercise
 
-function fullName(lastName, firstName, middleName) {
-    let fullname;
+// function fullName(lastName, firstName, middleName) {
+//     let fullname;
 
-    if (arguments.length == 2) {
-        fullname = `${firstName} ${lastName}`
-    } else {
-        fullname = `${firstName} ${middleName} ${lastName}`
-    }
-    console.log(fullname);
+//     if (arguments.length == 2) {
+//         fullname = `${firstName} ${lastName}`
+//     } else {
+//         fullname = `${firstName} ${middleName} ${lastName}`
+//     }
+//     console.log(fullname);
+// }
+
+// fullName("Potter", "Harry", "James");
+// fullName("Potter", "Harry");
+// fullName("Harry", "Potter");
+// fullName("Potter", "Harry", "James", "Pottypotpot");
+
+
+//////// Fourth exercise
+
+// function fullName(lastName, firstName, middleName) {
+//     let fullname;
+
+//     if (arguments.length == 2) {
+//         fullname = `${firstName} ${lastName}`
+//     } else {
+//         fullname = `${firstName} ${middleName} ${lastName}`
+//     }
+//     return(fullname);
+// }
+
+// const var1 = fullName("Potter", "Harry", "James");
+// const var2 = fullName("Potter", "Harry");
+// const var3 = fullName("Harry", "Potter");
+// const var4 = fullName("Potter", "Harry", "James", "Pottypotpot");
+
+// console.log(var1);
+// console.log(var2);
+// console.log(var3);
+// console.log(var4);
+
+/////// Fifth exercise
+
+const fullName = "Albus Percival Wulfric Brian Dumbledore";
+
+function getMiddleName(fullName) {
+    const middleNameStart = fullName.indexOf(" ");
+    const middleNameEnd = fullName.lastIndexOf(" ");
+
+    return fullName.substring(middleNameStart+1, middleNameEnd);
 }
 
-fullName("Potter", "Harry", "James");
-fullName("Potter", "Harry");
-fullName("Harry", "Potter");
-fullName("Potter", "Harry", "James", "Pottypotpot");
+console.log(getMiddleName(fullName));
